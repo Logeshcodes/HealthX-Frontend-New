5c7fff053c6d8aac41cadb3c5e9ec9dfeb8c9e6d
Logesh <logeshc18022002@gmail.com>
2025-03-23 23:42:23 +0530

    port fix cleaned

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "localhost",
+    host: true,
     port: 3000,
     proxy: {
       "/socket.io": {
-        target: "http://localhost:5006", // Your WebSocket backend
-        ws: true, // Enable WebSocket proxying
+        target: isProduction ? "https://healthx.live" : "http://localhost:5006",
+        ws: true,
         changeOrigin: true,
       },
     },
   },
-  preview: {  
-    port: 3000, 
+  preview: {
+    port: 3000,
   },
-  
 });
