772950d802738e9cc6c4bfc2e94650f0e4983db6
Logesh <logeshc18022002@gmail.com>
2025-03-27 09:14:19 +0530

    fix -socket-2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,6 @@
 // export const  VIDEO_SERVICE_DOMAIN = "http://localhost:5000";
 
 
-// socket connection
 export const  VIDEO_SERVICE_DOMAIN = "wss://healthx.live";
 
 export const SOCKET_URI = VIDEO_SERVICE_DOMAIN;
